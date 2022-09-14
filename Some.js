15d56dcd08f1ad40112f1109fc5b37e6821b7904
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-14 18:30:53 +0300

    Array

--- Some.js
+++ Some.js
@@ -1,3 +1,9 @@
 const numbers = [2, 4, 4, 5, 5];
 const res = numbers.some( e => e >= 5 );
 console.log( res );
+
+//Using the some method let's check if our array contain the given value
+function exist( value, array ) {
+    return array.some( e => e === value );
+}
+console.log( exist( 6, numbers ) );
