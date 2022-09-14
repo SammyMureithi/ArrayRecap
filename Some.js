0486f733f33500f93dad0d4b2646156ce80fee0e
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-14 18:36:22 +0300

    Array

--- Some.js
+++ Some.js
@@ -7,3 +7,19 @@ function exist( value, array ) {
     return array.some( e => e === value );
 }
 console.log( exist( 6, numbers ) );
+
+//Let's check if our array has an element in our range
+let ourRange = {
+    min: 0,
+    max: 5
+};
+let resRange = numbers.some( function ( e ) {
+    return e >= this.min && e <= this.max;
+}, ourRange );
+console.log( resRange );
+
+//lets take a closer look to empty array
+// with some it will always return false unlike every
+console.log( "Empty Array results" );
+let emptyRes = [].some( elment => elment > 0 );
+console.log( emptyRes );
