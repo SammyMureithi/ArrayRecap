e96dc69dab0f5589929b0ec7b064f1825eb8db92
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-14 18:07:17 +0300

    Array

--- Every.js
+++ Every.js
@@ -3,3 +3,12 @@ const isEven = numbers.every( number => {
     return number % 2 == 0;
 } );
 console.log( isEven );
+//suppose we have a ramge to check if our elements fit within the range
+let range = {
+    min: 0,
+    max: 5
+};
+const isInRange = numbers.every( function(number){
+    return number >= this.min && number <= this.max;
+}, range );
+console.log("Range "+ isInRange );
