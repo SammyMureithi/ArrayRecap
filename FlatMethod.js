de21bc5d8997b6348af988dc9c9f0726d7a82a9f
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-15 15:00:51 +0300

    Flat method

--- FlatMethod.js
+++ FlatMethod.js
@@ -2,3 +2,8 @@ const numbers = [1, 2, 3, [4, 5]];
 //we could make this flat using the flat method
 const flatNumbers = numbers.flat( 1 );
 console.log( flatNumbers );
+
+//we could also flatten an array that we don't know how deep its nested
+const deep = [1, 2, 3, [4, 5, [6, 7, [8, 9]]]];
+const flatDeep = deep.flat( Infinity );
+console.log( flatDeep );
