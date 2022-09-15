c8865278de11a86b2067c6526039fbd623600f49
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-15 19:35:15 +0300

    Join

--- Join.js
+++ Join.js
@@ -1,4 +1,10 @@
 const cssClasses = ['btn', 'btn-primary', 'btn-active'];
 const btnClass = cssClasses.join(' ');
 
-console.log(btnClass);
+console.log( btnClass );
+const title = 'JavaScript array join example';
+const url = title.split(' ')
+    .join('-')
+    .toLowerCase();
+
+console.log(url);
